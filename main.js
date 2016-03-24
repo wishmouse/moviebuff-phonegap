f30f917ad7587a83d17dc7a4fbcc2c09d2df1cdb
James Sangalli & Rahmona Henry <pair+james-sangalli.rahmona-henry@devbootcamp.com>
2016-03-25 11:54:54 +1300

    Project foundations

---
+++ main.js
@@ -0,0 +1,10 @@
+var request = require('superagent')
+var dotenv = require('dotenv')
+dotenv.load()
+
+//get search input from user...
+
+//call api with serach criteria....
+
+//get response back from api and render it to the user screen....
+
